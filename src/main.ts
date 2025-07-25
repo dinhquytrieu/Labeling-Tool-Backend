ed6f1febfec9e155da9c55777beeaccbdd8e255f
Dinh Quy Trieu <dinhquytrieu1010@gmail.com>
2025-07-26 01:41:57 +0700

    chore: clean code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +13,7 @@ async function bootstrap() {
   
   // Enable CORS for frontend communication
   app.enableCors({
-    origin: 'http://localhost:3002', // Frontend URL
+    origin: process.env.FRONTEND_URL || 'http://localhost:3002', // Frontend URL from .env
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
